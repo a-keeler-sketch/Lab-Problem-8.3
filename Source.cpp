0c60acec372a5412b64255408899e82da59339cc
Anna Keeler <a-keeler@onu.edu>
2025-10-27 19:49:30 -0400

    completed convertRomanToInt function.

--- Source.cpp
+++ Source.cpp
@@ -12,14 +12,15 @@ using namespace std;
 //prototype
 
 int romanCharValue(char r);
+int convertRomanToInt(string s);
 
 //global variable
 
-char input = 'X';
+string input = "MCMLXVIII";
 
 int main(void) {
 
-    cout << "Roman Numeral Value Equals: " << romanCharValue(input) << endl;
+    cout << "Roman Numeral Value Equals: " << convertRomanToInt(input) << endl;
     return 0;
 
 }
@@ -33,7 +34,7 @@ int romanCharValue(char r) {
     if (r == 'C') return 100;
     if (r == 'D') return 500;
     if (r == 'M') return 1000;
-    
+
 
 
 
@@ -41,4 +42,19 @@ int romanCharValue(char r) {
 
     return 0;
 
+}
+int convertRomanToInt(string s) {
+    int total = 0;
+    while (s.length() != 0) {
+        if (romanCharValue(s[0]) >= romanCharValue(s[1]) || s.length() == 1) {
+            total += romanCharValue(s[0]);
+            s = s.substr(1);
+        }
+        else {
+            total += romanCharValue(s[1]) - romanCharValue(s[0]);
+            s = s.substr(2);
+        }
+
+    }
+    return total;
 }
