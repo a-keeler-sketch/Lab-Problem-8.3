29bf11461f2a364df12fb3bed870fb809278cbdd
Anna Keeler <a-keeler@onu.edu>
2025-10-27 19:20:37 -0400

    Completed romanCharValue function.

--- Source.cpp
+++ Source.cpp
@@ -9,15 +9,31 @@
 #include <cstdlib>
 #include <ctime>
 using namespace std;
+//prototype
 
+int romanCharValue(char r);
+
+//global variable
+
+char input = 'X';
 
 int main(void) {
 
-   
+    cout << "Roman Numeral Value Equals: " << romanCharValue(input) << endl;
+    return 0;
 
 }
 
 int romanCharValue(char r) {
+
+    if (r == 'I') return 1;
+    if (r == 'V') return 5;
+    if (r == 'X') return 10;
+    if (r == 'L') return 50;
+    if (r == 'C') return 100;
+    if (r == 'D') return 500;
+    if (r == 'M') return 1000;
+    
 
 
 
